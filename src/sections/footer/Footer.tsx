762ba48acdeccc3c6ffcf30e22c0097c99f43377
Sanket Pande <sanketpande99001@gmail.com>
2024-08-31 16:01:02 +0530

    fix footer

--- src/sections/footer/Footer.tsx
+++ src/sections/footer/Footer.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer>
-            <p>© 2024 sanket pande. All rights reserved.</p>
+        <footer className={styles.footer}>
+            <nav>
+                <ul>
+                    <li>Sanket Pande</li>
+                </ul>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
         </footer>
     );
 }
